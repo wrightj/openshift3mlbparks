07b646504a05934d66bcb4f806ff9ba8bd77772e
Derrick Sutherland <dsutherland@shadow-soft.com>
2016-02-03 12:06:40 -0500

    Updated the postgres connection to debug better and fixed some error catching

--- src/main/java/org/openshift/mlbparks/postgresql/DBPostgreSQLConnection.java
+++ src/main/java/org/openshift/mlbparks/postgresql/DBPostgreSQLConnection.java
@@ -14,6 +14,8 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
@@ -27,6 +29,7 @@ import org.openshift.mlbparks.domain.MLBPark;
 @Named
 @ApplicationScoped
 public class DBPostgreSQLConnection {
+	private static Logger logger = Logger.getLogger(DBPostgreSQLConnection.class.getCanonicalName());
 	private Connection conn;
 
 	@PostConstruct
@@ -48,14 +51,14 @@ public class DBPostgreSQLConnection {
 			Class.forName("org.postgresql.Driver");
 			String url = "jdbc:postgresql://" + postgresqlHost + ":" + postgresqlPort + "/" + postgresqlDBName;
 			conn = DriverManager.getConnection(url, postgresqlUser, postgresqlPassword);
-			System.out.println("Connected to database");
+			logger.log(Level.INFO,"Connected to database");
 			connected=true;
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
-			System.out.println("Couldn't load the driver class: " + e.getMessage() + " :: " + e.getClass());
+			logger.log(Level.SEVERE,"Couldn't load the driver class: " + e.getMessage() + " :: " + e.getClass());
 
 		} catch (SQLException e) {
-			System.out.println("Couldn't connect to PostgreSQL: " + e.getMessage() + " :: " + e.getClass());
+			logger.log(Level.SEVERE,"Couldn't connect to PostgreSQL: " + e.getMessage() + " :: " + e.getClass());
 		}
 		
 		if(connected)
@@ -66,7 +69,7 @@ public class DBPostgreSQLConnection {
 	private void initDatabase() {
 		int teamsImported = 0;
 		if (conn!=null && getMLBParks().isEmpty()) {
-			System.out.println("The database is empty.  We need to populate it");
+			logger.log(Level.SEVERE,"The database is empty.  We need to populate it");
 			JSONParser jsonParser = new JSONParser();
 			PreparedStatement st = null;
 			ResultSet rs = null;
@@ -116,7 +119,7 @@ public class DBPostgreSQLConnection {
 					st.close();
 					teamsImported++;
 				}
-				System.out.println("Successfully imported " + teamsImported + " teams.");
+				logger.log(Level.FINE,"Successfully imported " + teamsImported + " teams.");
 
 			} catch (Exception e) {
 				e.printStackTrace();
@@ -124,11 +127,10 @@ public class DBPostgreSQLConnection {
 				try {
 					rs.close();
 					st.close();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
+				} catch (Exception e) {}
 			}
-		}
+		} else
+			logger.log(Level.INFO,"The database is already configured.");
 	}
 
 	public Connection getConnection() throws Exception {
@@ -140,60 +142,36 @@ public class DBPostgreSQLConnection {
 	}
 
 	public ArrayList<MLBPark> getMLBParks() {
-		ArrayList<MLBPark> result = new ArrayList<MLBPark>();
-		Statement st = null;
-		ResultSet rs = null;
-		try {
-			st = conn.createStatement();
-			rs = st.executeQuery("SELECT id,name,lat,long,ballpark,league,payroll FROM mlbparks ORDER BY id");
-
-			while (rs.next()) {
-				MLBPark mlbpark = new MLBPark();
-				mlbpark.setId(rs.getString("id"));
-				mlbpark.setName(rs.getString("name"));
-				mlbpark.setPosition(this.processCoordinates(rs.getString("coordinates")));
-				mlbpark.setBallpark(rs.getString("ballpark"));
-				mlbpark.setLeague(rs.getString("league"));
-				mlbpark.setPayroll(rs.getString("payroll"));
-
-				result.add(mlbpark);
-			}
-		} catch (SQLException se) {
-			System.err.println("Threw a SQLException creating the list of blogs.");
-			System.err.println(se.getMessage());
-		} finally {
-			try {
-				rs.close();
-				st.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		return result;
+		return getMLBParks(false,0,0,0,0);
 
 	}
 	
-	public ArrayList<MLBPark> getMLBParksWithRange(float lat1,float lon1, float lat2,float lon2)
+	public ArrayList<MLBPark> getMLBParks(boolean range,float lat1,float lon1, float lat2,float lon2)
 	{
 		ArrayList<MLBPark> result = new ArrayList<MLBPark>();
 		PreparedStatement st = null;
 		ResultSet rs = null;
 		try {
-			st = conn.prepareStatement("SELECT id,name,lat,long,ballpark,league,payroll "
-					+ "FROM mlbparks "
-					+ "where lat <=? AND lat >=? AND "
-					+ "long <=? AND long >=? "
-					+ "ORDER BY id");
-			st.setDouble(1, lat1);
-			st.setDouble(2, lat2);
-			st.setDouble(3, lon1);
-			st.setDouble(4, lon2);
+			String statement="SELECT id,name,lat,long,ballpark,league,payroll "
+					+ "FROM mlbparks ";
+			if(range)
+				statement=statement+"where lat <=? AND lat >=? AND long <=? AND long >=? ";
 			
-			System.out.println("North lat is: "+lat1);
-			System.out.println("South lat is: "+lat2);
-			System.out.println("East long is: "+lon1);
-			System.out.println("West long is: "+lon2);
+			statement=statement+ "ORDER BY id";
 			
+			st = conn.prepareStatement(statement);
+			
+			if(range)
+			{
+				logger.log(Level.FINE,"North lat is: "+lat1);
+				logger.log(Level.FINE,"South lat is: "+lat2);
+				logger.log(Level.FINE,"East long is: "+lon1);
+				logger.log(Level.FINE,"West long is: "+lon2);
+				st.setDouble(1, lat1);
+				st.setDouble(2, lat2);
+				st.setDouble(3, lon1);
+				st.setDouble(4, lon2);
+			}		
 			rs = st.executeQuery();
 			
 			while (rs.next()) {
@@ -207,16 +185,14 @@ public class DBPostgreSQLConnection {
 
 				result.add(mlbpark);
 			}
-		} catch (SQLException se) {
-			System.err.println("Threw a SQLException creating the list of blogs.");
-			System.err.println(se.getMessage());
+		} catch (Exception se) {
+			logger.log(Level.SEVERE,"Threw an Exception getting the list MLB Parks.");
+			logger.log(Level.SEVERE,se.getMessage());
 		} finally {
 			try {
 				rs.close();
 				st.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
+			} catch (SQLException e) {}
 		}
 		return result;
 	}
@@ -225,22 +201,20 @@ public class DBPostgreSQLConnection {
 		Map<String,List<Double>> result = new HashMap<String,List<Double>>();
 		String[] coordinates= coord.split(":");
 		for (int i = 0; i < coordinates.length; i++) {
-			System.out.println(coordinates[i]);
+			logger.log(Level.FINE,coordinates[i]);
 		}
 		List<Double> c = new ArrayList<Double>();
 		c.add(Double.valueOf(coordinates[0]));
 		c.add(Double.valueOf(coordinates[1]));
 		result.put("coordinates",c);
 		return result;
-		
-		
+				
 	}
-	
 	
 	//TODO on postgresql the coord record should be persisted like it's shown below 
 	public static void main(String []args){
 		DBPostgreSQLConnection c = new DBPostgreSQLConnection();
 		Map<String,List<Double>> t= c.processCoordinates("coordinates:-84.38839:33.734708");
-		System.out.println("Result"+t);
+		logger.log(Level.FINE,"Result"+t);
 	}
 }
